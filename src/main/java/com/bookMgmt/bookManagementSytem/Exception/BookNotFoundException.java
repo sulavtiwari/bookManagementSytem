c6715fde4f66461d7ce727d7fa77a57a612e2959
sulavtiwari <sulavtiwari55@gmail.com>
2023-09-17 12:22:03 +0545

    made GlobalException Class

--- src/main/java/com/bookMgmt/bookManagementSytem/Exception/BookNotFoundException.java
+++ src/main/java/com/bookMgmt/bookManagementSytem/Exception/BookNotFoundException.java
@@ -1,4 +1,7 @@
 package com.bookMgmt.bookManagementSytem.Exception;
 
-public class BookNotFoundException {
+public class BookNotFoundException extends RuntimeException{
+    public BookNotFoundException(String msg){
+        super(msg);
+    }
 }
